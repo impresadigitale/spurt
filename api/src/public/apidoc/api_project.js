51130d5ce7945911d3563156b95de952305f3a10
carlo <carloiodice42@gmail.com>
2021-05-13 01:33:23 +0200

    leggere commit.txt

--- api/src/public/apidoc/api_project.js
+++ api/src/public/apidoc/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-05-12T11:20:34.376Z",
+    "time": "2021-05-12T19:27:13.653Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
---
+++ store/src/default/pages/compriamo-auto/compriamo-auto.module.ts
@@ -0,0 +1,41 @@
+/*
+ * spurtcommerce
+ * version 4.4
+ * http://www.spurtcommerce.com
+ *
+ * Copyright (c) 2021 piccosoft ltd
+ * Author piccosoft ltd <support@piccosoft.com>
+ * Licensed under the MIT license.
+ */
+// module
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {ReactiveFormsModule} from '@angular/forms';
+import {SharedModule} from '../../shared/shared.module';
+import {ComponentsModule} from '../../shared/components/index';
+import {CompriamoAutoComponent} from './compriamo-auto.component';
+import {TranslateModule} from '@ngx-translate/core';
+import {NumberAcceptModule} from '../../shared/validation-directives/onlyNumber.module';
+
+
+export const routes = [
+    {path: '', component: CompriamoAutoComponent, pathMatch: 'full'}
+];
+
+@NgModule({
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes),
+        ReactiveFormsModule,
+        SharedModule,
+        ComponentsModule,
+        TranslateModule.forChild(),
+        NumberAcceptModule
+    ],
+    declarations: [
+        CompriamoAutoComponent
+    ]
+})
+export class ContactModule {
+}
