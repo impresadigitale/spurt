16f03c793993448cd3a5b135fbe0f96bf5fcd5f4
ImpresaDigitale <incarnato@libero.it>
2021-05-17 16:22:53 +0200

    Carlo_17_05_21.txt

---
+++ api/src/api/controllers/FinController.ts
@@ -0,0 +1,44 @@
+
+import 'reflect-metadata';
+import {
+    Get,
+    JsonController,
+    Authorized,
+    QueryParam,
+    Res,
+    Body,
+    Req,
+    Post,
+    Param,
+    Put, Delete, UploadedFile, BodyParam
+} from 'routing-controllers';
+
+@JsonController('/finaziamento')
+export class FinController {
+    constructor(){
+
+    }
+
+    @Post('/finaziamento/add')
+    @Authorized()
+    public async addFin(@Body({ validate: true} finaziamento: AddFinRequest, @Res() response: any): Promise<any>){
+        const newFin: any = new Finanziamento();
+        const saveFin = await this.finService.create(newFin);
+
+        if (saveFin) {
+            const successResponse: any = {
+                status: 1,
+                message: 'Finanziamento creato',
+                data: saveFin
+            };
+            return response.status(200).send(successResponse);
+        }
+        else {
+            const errorResponse: any = {
+                status: 0,
+                message: 'Finanziamento non creato',
+            };
+            return response.status(400).send(errorResponse);
+        }
+    }
+}
